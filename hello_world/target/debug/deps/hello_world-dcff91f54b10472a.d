cd06b28e37977627a7ae6b8756ffafd04d53148a
davidho27941 <jimsam3456@cycu.org.tw>
2021-08-10 23:26:35 +0800

    feat: the hello workd example of RUST.

---
+++ hello_world/target/debug/deps/hello_world-dcff91f54b10472a.d
@@ -0,0 +1,5 @@
+/home/david/workplace/Learning/RUST_learning/hello_world/target/debug/deps/hello_world-dcff91f54b10472a: src/main.rs
+
+/home/david/workplace/Learning/RUST_learning/hello_world/target/debug/deps/hello_world-dcff91f54b10472a.d: src/main.rs
+
+src/main.rs:
